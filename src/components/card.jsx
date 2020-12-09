ad7e16ec9a11f7d4935d8928cec95073f8bbc2ae
jtpevehouse <josh.pevehouse@gmail.com>
2020-12-09 09:32:52 -0600

    Cards loading as images

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import "./card.css";
 
-const Card = (props) => {
-  const { data: card } = props;
+const Card = ({ data: card }) => {
+  const faceId =
+    typeof card.face === "string" ? card.face.charAt(0) : card.face;
+  const suitId = card.suit.charAt(0).toUpperCase();
+
+  const cardUrl = `/img/cards/${faceId}${suitId}.png`;
 
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col">{card.face}</div>
-        <div className="col">{card.suit}</div>
-        <div className="col">({card.value})</div>
-      </div>
-    </div>
+    <img
+      className="playing-card"
+      src={window.location.origin + cardUrl}
+      alt={`${card.face} of ${card.suit}`}
+    />
   );
 };
 
